185820b8859293b377ebf9813aed8b4bd0c7bea0
Leon Gell <lgell@lcs.on.ca>
2022-11-15 14:41:18 -0500

    Initial Commit

---
+++ StopWatchInterface/StopWatchInterfaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  StopWatchInterfaceApp.swift
+//  StopWatchInterface
+//
+//  Created by Leon Gell on 2022-11-15.
+//
+
+import SwiftUI
+
+@main
+struct StopWatchInterfaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
